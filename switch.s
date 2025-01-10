7359877d343f116a56d8ffab8f97f1ca58674f1b
Aman <48176184+ZedRoff@users.noreply.github.com>
2025-01-10 18:45:23 +0100

    Update switch.s

--- switch.s
+++ switch.s
@@ -2,23 +2,23 @@
 	;;;;;;;;;;;; SETUP SWITCH
 	
 	AREA |.text|, CODE, READONLY
-GPIO_I_PUR EQU 0x510 
-GPIO_PORTD_BASE		EQU	0x40007000	
-GPIO_O_DEN EQU 0x51C
+GPIO_I_PUR EQU 0x510 ; Pull up resistor 
+GPIO_PORTD_BASE		EQU	0x40007000	; Port D de base pour les switchs
+GPIO_O_DEN EQU 0x51C ; Digital Enable
 BROCHE6				EQU 	0x40		; bouton poussoir 1
-BROCHE7 EQU 0x80
+BROCHE7 EQU 0x80 ; bouton poussoir 2
 	ENTRY
 	
-	EXPORT SWITCH_INIT
+	EXPORT SWITCH_INIT ; on exporte l'étiquette
 
 SWITCH_INIT
 
-	ldr r7, = GPIO_PORTD_BASE+GPIO_I_PUR	;; Pul_up 
-    ldr r0, =(BROCHE6+BROCHE7)
-    str r0, [r7]
+	ldr r7, = GPIO_PORTD_BASE+GPIO_I_PUR	;; Adresse du pull up resistor du portD
+    ldr r0, =(BROCHE6+BROCHE7) ; on stock l'addition
+    str r0, [r7] ; on écrit dans la mémoire pour activer le PUR des deux switchs
 		
-	ldr r7, = GPIO_PORTD_BASE+GPIO_O_DEN	;; Enable Digital Function 
-    ldr r0, =(BROCHE6+BROCHE7)	
-    str r0, [r7]     
+	ldr r7, = GPIO_PORTD_BASE+GPIO_O_DEN	;; Activé la fonction digitale 
+    ldr r0, =(BROCHE6+BROCHE7)	; on stock l'addition
+    str r0, [r7]   ; on écrit dans la mémoire pour activer le DEN des deux switchs  
 	BX LR
 		
